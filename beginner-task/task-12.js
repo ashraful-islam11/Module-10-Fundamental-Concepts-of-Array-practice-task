3577e3e49ab7653825e72bb2666a1e260d1a0238
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 16:19:07 +0600

    task 12 is done

---
+++ beginner-task/task-12.js
@@ -0,0 +1,11 @@
+// todo:        task-12         topic:   Loop through the array with a while loop and log each item.
+
+const numbers =[ 10, 20, 30, 40, 50 ,60, 70 ,80, 90];
+let start = 0;
+
+while(start < numbers.length){
+      console.log(numbers[start]);
+    
+    start++;
+
+}
