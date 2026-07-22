6e8c8a9e664e60360848ef58800e60dc72c62ca2
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 12:29:21 +0600

    task no 3 is done

---
+++ beginner-task/task-3.js
@@ -0,0 +1,8 @@
+// todo:         task-3      topic:  Log the item at index 0 and index 2 of the array.
+
+const number = [ 10, 20, 30];
+const zeroIndex = number[0];
+const secondIndex= number[2];
+
+console.log('o index value of :', zeroIndex);
+console.log('2nd  index value of :', secondIndex);
