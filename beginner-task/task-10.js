e256e5512522d746b8022d9fae5b7580af6dddf7
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 16:02:02 +0600

    task no -10 is done

---
+++ beginner-task/task-10.js
@@ -0,0 +1,7 @@
+// todo:    task-10         topic: Find the position of a specific value in the array. Log the number..
+
+const numbers =[ 20, 20,45,45, 90,30];
+console.log(numbers);
+
+ const findThePosition= numbers.indexOf(30);
+console.log('the specific value position is ',findThePosition);
