0c014bf1ffa5ec34619fbdd11965e7554a4c3a61
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 18:15:07 +0600

    task-2 is done

---
+++ entermediate/task-3.js
@@ -0,0 +1,6 @@
+// todo:    task-3          topic:Turn an array of words into a single sentence string.Expected: a string like "word word word".
+
+const words =[ 'word', 'word', 'word'];
+console.log(words );
+
+console.log(words.join(' '));
