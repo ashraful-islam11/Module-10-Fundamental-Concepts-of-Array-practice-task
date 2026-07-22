515a272e1646c229e5905e227b7f1684bbd9d749
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 18:49:46 +0600

    task-6 is done

---
+++ entermediate/task-6.js
@@ -0,0 +1,4 @@
+// todo:        task-6      topic: Sort an array of numbers in ascending order, keeping numeric order correct (not string order)..
+
+const number = [ 9,6,8,2,3,5,1,7];
+console.log(number.sort());
