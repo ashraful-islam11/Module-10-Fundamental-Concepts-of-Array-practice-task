d3ae69d201c84dd48b5e5866b7f4c7de206332ca
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 12:42:27 +0600

    task-5 is done

---
+++ beginner-task/task-5.js
@@ -0,0 +1,8 @@
+// todo:        task-5      topic: Add one item to the end of the array. Log the array.
+
+const number = [10, 20, 30, 40 ];
+console.log('the main array :',number);
+
+// add one the last item to the end of array. 
+number.push(50);
+console.log('the value after pushing:', number);
