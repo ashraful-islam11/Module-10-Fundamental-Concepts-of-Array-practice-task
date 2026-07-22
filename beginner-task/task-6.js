579284d2d27a5adb330058ceae0af1e3d6734c2d
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 12:51:13 +0600

    task-6 is done

---
+++ beginner-task/task-6.js
@@ -0,0 +1,10 @@
+// todo:        task-6.         topic:Remove the first item from the array. Log the array and the removed value.
+
+const number= [10, 20 ,30, 40];
+console.log('our main Array: ', number);
+
+ const firstRemove =number.shift();
+console.log('removed the first value and print  Array', number);
+
+const removedValue = firstRemove;
+console.log('this is the removed items' ,removedValue);
