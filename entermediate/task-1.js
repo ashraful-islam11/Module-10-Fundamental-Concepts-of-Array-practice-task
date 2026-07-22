8f4ae8d04e99d831c2be11bcfb9a98e8d1bc1d6f
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 17:01:12 +0600

    task-1 is done

---
+++ entermediate/task-1.js
@@ -0,0 +1,6 @@
+// todo:        task-1          topic:  Combine two arrays into one new array. Log the result.
+
+const numbers= [ 10, 20, 30];
+const age =[ 19, 20, 21];
+
+console.log(numbers.concat(age));
