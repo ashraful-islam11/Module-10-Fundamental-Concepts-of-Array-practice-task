923af99d5e23c5787c649e0bffdd4bd26f997ba7
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 17:10:24 +0600

    task-2 is done

---
+++ entermediate/task-2.js
@@ -0,0 +1,11 @@
+// todo:            task-2      topic:  Take a portion of an array (not the whole thing) without changing the original. Log the result.
+
+
+const numbers= [ 12,33,44,55,66,77,77,88,99,17];
+
+console.log('oreginal array of value',numbers);
+
+const willSlice = numbers.slice( 2, 8);
+console.log('after the slice',willSlice);
+
+console.log('original array of value', numbers);
