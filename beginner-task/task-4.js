853e49726236ba15c22d94abc99efce271380afd
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 12:34:35 +0600

    task-4 is done

---
+++ beginner-task/task-4.js
@@ -0,0 +1,9 @@
+// todo:         Task - 4.        topic: Change the item at index 1 to a new value. Log the array.
+
+const number =[ 10, 20, 30, 50 ];
+console.log('original array', number);
+
+// change 1 index of value.
+
+number[1] = 100;
+console.log('change index 1 of value', number);
